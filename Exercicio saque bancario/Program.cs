f84c6724c5d8fd7f50118bcdcbf0176a3526e2e8
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-09 16:11:34 -0300

    exercicio finalizado

---
+++ Exercicio saque bancario/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_saque_bancario.Entities;
+using Exercicio_saque_bancario.Entities.Exceptions;
+
+
+
+namespace Exercicio_saque_bancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //tem um erro no código, roda e compara 
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holdere = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holdere, balance, withdrawLimit);
+
+            Console.WriteLine("\nEnter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
